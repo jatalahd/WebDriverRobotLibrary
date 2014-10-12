59c6e17101f04a989e0367a586b0c4a759ab22d6
jatalahd <jatalahd@gmail.com>
2014-10-12 13:40:39 +0300

    updated to use the newest webdriver release and library scope changed from GLOBAL to TEST SUITE

--- src/main/java/WebDriverRobotLibrary.java
+++ src/main/java/WebDriverRobotLibrary.java
@@ -1,7 +1,7 @@
 import org.robotframework.javalib.library.AnnotationLibrary;
 
 public class WebDriverRobotLibrary extends AnnotationLibrary {
-    public static final String ROBOT_LIBRARY_SCOPE = "GLOBAL";
+    public static final String ROBOT_LIBRARY_SCOPE = "TEST SUITE";
     public static final String ROBOT_LIBRARY_VERSION = "1.0";
     
     public WebDriverRobotLibrary() {
